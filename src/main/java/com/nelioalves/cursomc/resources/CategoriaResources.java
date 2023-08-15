69c76e93652219a5a15802c2cdb6fc1020029436
Thiago Prado <thiagopradodemellobranco@MacBook-Pro-de-Thiago.local>
2023-08-15 15:14:25 -0300

    Criando repository e service para Categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResources.java
@@ -1,31 +1,29 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping (value = "/categorias")
-
 public class CategoriaResources {
 	
-	@RequestMapping (method = RequestMethod.GET) // mapea para requisicoes get de categoria 
+	@Autowired
+	private CategoriaService service;
 	
-	public List<Categoria> Listar() {
+	@RequestMapping (value="/{id}", method=RequestMethod.GET) // mapea para requisicoes get de categoria 
+	
+	
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);	
 		
-		Categoria cat1 = new Categoria (1,"informatica"); // instancia objeto cat1
-		Categoria cat2 = new Categoria (2,"Escritorio");	
 		
-		List<Categoria> lista = new ArrayList<>(); // cria lista tipo cateria e o arraylist
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista; // retorna lista 
 	}
 }
---
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+		public Categoria buscar(Integer id) {
+			Optional<Categoria> obj = repo.findById(id);
+			return obj.orElse(null);
+		
+	}
+	
+}
